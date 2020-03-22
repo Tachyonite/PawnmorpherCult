0db13e27864101b762b490f1aa2116ed0ffca6ef
FxWolfxe <ndmuench@comcast.net>
2020-03-21 20:31:03 -0400

    set up visual studios project

---
+++ Source/PawnmorpherCult/PawnmorpherCult/ModInit.cs
@@ -0,0 +1,19 @@
+﻿// ModInit.cs created by Iron Wolf for PawnmorpherCult on 03/21/2020 8:23 PM
+// last updated 03/21/2020  8:23 PM
+
+using Verse;
+
+namespace PawnmorpherCult
+{
+    /// <summary>
+    /// static class where most of the mod's initialization should happen 
+    /// </summary>
+    [StaticConstructorOnStartup]
+    public static class ModInit
+    {
+        static ModInit()
+        {
+           
+        }
+    }
+}
---
+++ Source/PawnmorpherCult/PawnmorpherCult/PMCult.cs
@@ -0,0 +1,16 @@
+﻿// PMCult.cs created by Iron Wolf for PawnmorpherCult on 03/21/2020 8:22 PM
+// last updated 03/21/2020  8:22 PM
+
+using Verse;
+
+namespace PawnmorpherCult
+{
+    public class PMCult : Mod
+    {
+        public PMCult(ModContentPack content) : base(content)
+        {
+        }
+
+        
+    }
+}
