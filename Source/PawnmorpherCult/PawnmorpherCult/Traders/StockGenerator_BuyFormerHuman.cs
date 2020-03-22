10c670ed91ecb1b9a7b2358d192c4467e16ca78c
FxWolfxe <ndmuench@comcast.net>
2020-03-22 17:06:29 -0400

    added a bunch of trader stuff for the cult faction

---
+++ Source/PawnmorpherCult/PawnmorpherCult/Traders/StockGenerator_BuyFormerHuman.cs
@@ -0,0 +1,45 @@
+﻿// StockGenerator_BuyFormerHuman.cs created by Iron Wolf for PawnmorpherCult on 03/22/2020 4:34 PM
+// last updated 03/22/2020  4:34 PM
+
+using System.Collections.Generic;
+using System.Linq;
+using Pawnmorph;
+using RimWorld;
+using Verse;
+
+namespace PawnmorpherCult.Traders
+{
+    //TODO figure out a way to restrict it to former humans only 
+
+    public class StockGenerator_BuyFormerHuman : StockGenerator
+    {
+        public SapienceLevel minSapienceLevel;
+        public override IEnumerable<Thing> GenerateThings(int forTile, Faction faction = null)
+        {
+            return Enumerable.Empty<Thing>();
+        }
+
+        public override bool HandlesThingDef(ThingDef thingDef)
+        {
+            return thingDef.race?.Animal == true;
+        }
+
+
+        
+    }
+
+
+    public class StockGenerator_BuyChaomorph : StockGenerator
+    {
+        public override IEnumerable<Thing> GenerateThings(int forTile, Faction faction = null)
+        {
+            return Enumerable.Empty<Thing>();
+        }
+
+        public override bool HandlesThingDef(ThingDef thingDef)
+        {
+            return thingDef?.tradeTags?.Contains("Chaomorph") == true; 
+        }
+    }
+    
+}
