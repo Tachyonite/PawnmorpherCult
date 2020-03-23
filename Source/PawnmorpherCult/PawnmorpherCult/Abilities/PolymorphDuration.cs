91627f9a8065740027b039baabfdddc228fb03b1
FxWolfxe <ndmuench@comcast.net>
2020-03-22 23:07:16 -0400

    Made polymorph wear off after awhile

---
+++ Source/PawnmorpherCult/PawnmorpherCult/Abilities/PolymorphDuration.cs
@@ -0,0 +1,72 @@
+﻿// PolymorphDuration.cs created by Iron Wolf for PawnmorpherCult on 03/22/2020 10:28 PM
+// last updated 03/22/2020  10:28 PM
+
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Pawnmorph;
+using Pawnmorph.TfSys;
+using Pawnmorph.Utilities;
+using Verse;
+
+namespace PawnmorpherCult.Abilities
+{
+    public class PolymorphDurationComp : AbilityCompBase<PolymorphDurationProperties>
+    {
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+
+            var transformationRequest = new TransformationRequest(Properties.TargetAnimals.RandElement(), target.Pawn);
+
+            var tfdPawn = MutagenDefOf.defaultMutagen.MutagenCached.Transform(transformationRequest);
+            var animal = tfdPawn?.TransformedPawns.First(); 
+
+            if (tfdPawn != null)
+            {
+                Find.World.GetComponent<PawnmorphGameComp>().AddTransformedPawn(tfdPawn); 
+                Hediff hediff = HediffMaker.MakeHediff(Properties.trackerHediff, animal);
+                var rmComp = hediff.TryGetComp<HediffComp_Disappears>();
+
+                rmComp.ticksToDisappear = GetDurationSeconds(target.Pawn).SecondsToTicks();
+                animal.health.AddHediff(hediff); 
+            }
+            else
+            {
+                Log.Warning($"could not transform target {target.Pawn.Label}");
+            }
+        }
+
+        public override bool Valid(LocalTargetInfo target, bool throwMessages = false)
+        {
+            if (target.Pawn == null || !MutagenDefOf.defaultMutagen.CanTransform(target.Pawn))
+                return false;
+            return base.Valid(target, throwMessages);
+        }
+    }
+
+    public class PolymorphDurationProperties : AbilityCompPropertiesBase<PolymorphDurationComp>
+    {
+        public List<PawnKindDef> targetAnimals;
+        public HediffDef trackerHediff;
+
+        private List<PawnKindDef> _targetsCached;
+
+        [NotNull]
+        public List<PawnKindDef> TargetAnimals
+        {
+            get
+            {
+                if (_targetsCached == null)
+                {
+                    if (targetAnimals != null)
+                        _targetsCached = targetAnimals;
+                    else
+                        _targetsCached = DefDatabase<PawnKindDef>.AllDefsListForReading.Where(d => d.RaceProps.Animal).ToList();
+                }
+
+                return _targetsCached;
+            }
+        }
+    }
+}
---
+++ Source/PawnmorpherCult/PawnmorpherCult/Hediffs/RemovePolymorphComp.cs
@@ -0,0 +1,33 @@
+﻿// RemovePolymorphComp.cs created by Iron Wolf for PawnmorpherCult on 03/22/2020 10:35 PM
+// last updated 03/22/2020  10:35 PM
+
+using Pawnmorph;
+using Pawnmorph.TfSys;
+using Verse;
+
+namespace PawnmorpherCult.Hediffs
+{
+    public class RemovePolymorphComp : HediffComp_Disappears
+    {
+
+        public override void CompPostPostRemoved()
+        {
+            base.CompPostPostRemoved();
+            if (Pawn.Dead) return; 
+            if(Find.World.GetComponent<PawnmorphGameComp>().GetPawnStatus(parent.pawn) == TransformedStatus.Transformed)
+                MutagenDefOf.defaultMutagen.MutagenCached.TryRevert(parent.pawn); 
+            else 
+                Log.Warning($"{parent.def.defName} on {parent.pawn.Name} but that pawn is not a former human!");
+        }
+
+     
+    }
+
+    public class RemovePolymorphCompProperties : HediffCompProperties_Disappears
+    {
+        public RemovePolymorphCompProperties()
+        {
+            compClass = typeof(RemovePolymorphComp); 
+        }
+    }
+}
